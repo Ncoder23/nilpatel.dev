0cf090df215bbdad7de11f8e90aaf1a2e8e040df
Ncoder <nil171823@gmail.com>
2025-01-25 18:10:30 -0800

    deploying

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'; // Import the SVGR plugin
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), ghPages()],
+   base: "/nilpatel.dev"
 })
